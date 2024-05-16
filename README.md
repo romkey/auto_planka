eed5bbc5dedbaeaad103a0c2311ea3c1d81fa077
John Romkey <58883+romkey@users.noreply.github.com>
2024-05-16 16:04:07 -0700

    updated README
    changed to use public projects rather than boards
    
    propogate labels across all boards in public projects
    
    make Planka admins managers for all public projects

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # auto_planka
 
-Automatically add new users to "public" boards.
+The auto_planka script makes a few changes to the Planka database to better suit its use at PDX Hackerspace.
 
-[Planka](https://github.com/plankanban/planka) doesn't currently (v1.72.2) support "public" boards. At PDX Hackerspace we want to have some boards which automatically have all users added to them.
+Planka doesn't currently support "public" boards or projects though this appears to be on its roadmap.
 
-This script takes a simple JSON file that lists the IDs of public boards and once per minute will attempt to add all users to the boards. This works for our use case; we have few enough users that the overhead is low for us.
+At PDX Hackerspace we're using Planka as a kanban board to share projects with our members. While we want to allow for
+private projects, some will be "public" - accessible to all members.
 
+1. We want to have "public" projects whose boards automatically have all members added to them
+2. All Planka admins should automatically be managers of boards in public projects
+3. Labels on any board in a public project should be available on all other boards in public projects
+
+This script takes a simple JSON file that lists the IDs of public projects and once per minute will attempt to add all users to the boards.
+This works for our use case; we have few enough users that the overhead is low for us.
+
+To make labels work properly this SQL command should be run on the Planka database:
+```
+ALTER TABLE label ADD UNIQUE ( name, board_id);
+```
+
+This disallows multiple labels with the same name on a board, greatly simplifying the `auto_planka` script.
+
+In lieu of Planka supporting this functionality directly it would no doubt be better implemented directly in the database using stored
+procedures and triggers.
--- app/auto_planka.rb
+++ app/auto_planka.rb
@@ -4,30 +4,78 @@ require 'pg'
 require 'json'
 require 'dotenv/load'
 
+def _to_sql_list(array)
+  array.map { |elt| "'#{elt}'" }.join(',')
+end
+
+def _get_public_board_ids(config, db)
+  q = "SELECT * FROM board WHERE project_id IN ( #{_to_sql_list(config[:public_project_ids])} )"
+  boards = db.exec(q)
+  boards.map { |board| board['id'] }
+end
+
+# make_board_public by adding all users to a board
+def make_projects_public(config, db, timestamp)
+  users = db.exec("SELECT * FROM user_account")
+
+  config[:public_project_ids].each do |project_id|
+    boards_query = db.exec("SELECT * FROM board WHERE project_id = #{project_id}")
+    boards_query.each do |board|
+      board_id = board['id']
+      users.each do |user|
+        user_id = user['id']
+        db.exec("INSERT INTO board_membership VALUES (next_id(), '#{board_id}', '#{user_id}', '#{timestamp}', NULL, 'editor', NULL) ON CONFLICT DO NOTHING");
+      end
+    end
+  end
+end
+
+# make_labels_public by adding every label from all boards in a project
+# that contains public boards 
+def make_labels_public(config, db, timestamp, public_board_ids)
+  labels = db.exec("SELECT DISTINCT ON ( name ) * FROM label WHERE board_id IN ( #{_to_sql_list(public_board_ids)}) ORDER BY name, created_at");
+  labels.each do |label|
+    public_board_ids.each do |board_id|
+      db.exec("INSERT INTO label VALUES ( next_id(), '#{board_id}', '#{label["name"]}', '#{label["color"]}', '#{timestamp}', NULL, '#{label["position"]}') ON CONFLICT DO NOTHING")
+    end
+  end
+end
+
+# make_admins_public_project_managers by adding all Planka admin
+# accounts as managers on each project that contains public boards
+def make_admins_public_project_managers(config, db, timestamp)
+  admins = db.exec("SELECT * FROM user_account WHERE is_admin = 't'")
+
+  config[:public_project_ids].each do |project_id|
+    admins.each do |admin|
+      admin_id = admin["id"]
+      db.exec("INSERT INTO project_manager VALUES (next_id(), '#{project_id}', '#{admin_id}', '#{timestamp}', NULL) ON CONFLICT DO NOTHING")
+    end
+  end
+end
+
 db = PG.connect(ENV['POSTGRESQL'])
 
 config = JSON.parse(File.read('config.json'), symbolize_names: true)
 
-results = db.exec("SELECT * from BOARD")
-results.each do |row|
-  puts row['name'], row['id']
-end
+#results = db.exec("SELECT * from BOARD")
+#results.each do |row|
+#  puts row['name'], row['id']
+#end
 
 loop do 
-  users = db.exec("SELECT * from USER_ACCOUNT")
-#  users.each do |row|
-#    puts row['name']
-#  end
-
   timestamp = Time.now.strftime('%Y-%m-%d %H:%M:%S.%L')
 
-  config[:public_board_ids].each do |board_id|
-    puts "checking board #{board_id}"
-    users.each do |user|
-      user_id = user['id']
-      db.exec("INSERT INTO board_membership VALUES (next_id(), '#{board_id}', '#{user_id}', '#{timestamp}', NULL, 'editor', NULL) ON CONFLICT DO NOTHING");
-    end
-  end
+  public_board_ids = _get_public_board_ids(config, db)
+
+  make_projects_public(config, db, timestamp)
+  puts 'made projects public'
+
+  make_admins_public_project_managers(config, db, timestamp)
+  puts 'made admins managers'
+
+  make_labels_public(config, db, timestamp, public_board_ids)
+  puts 'propogated labels'
 
   sleep(60)
 end
